5c62081363da50f22db8290df3254828afd8a92e
Siddharth Rajit <siddharth.rajit2207@gmail.com>
2026-04-03 19:10:20 -0300

    add login route to authentication router

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { register } from "../controllers/auth.controller";
+import { login, register } from "../controllers/auth.controller";
 
 const router = Router();
 
 // Public
 
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;
