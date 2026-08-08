28e114b222f484db80a0e187a01fdc767e31c6ec
Siddharth Rajit <siddharth.rajit2207@gmail.com>
2026-08-08 19:17:00 -0300

    Initial Commit.Folder structure created.

---
+++ src/test/java/org/demo/authsystem/AuthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.authsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
